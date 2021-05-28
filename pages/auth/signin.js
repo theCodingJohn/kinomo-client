8eb3f1d30a53261c362421929aa02724ea13d501
John Carlo Cunanan <codingjohn@outlook.com>
2021-05-28 17:16:19 +0800

    Finish login page styling

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 
@@ -8,6 +9,8 @@ import schema from "../../utils/yupSchema";
 import styles from "../../styles/signin.module.scss";
 
 const login = () => {
+  const [loginError, setLoginError] = useState(null);
+
   const {
     register,
     handleSubmit,
@@ -16,15 +19,37 @@ const login = () => {
     resolver: yupResolver(schema),
   });
 
-  const onSubmit = async (data) => {
+  const onSubmit = async (data, e) => {
     const user = await authService.signin(data);
-    console.log(user);
+
+    if (user.error) {
+      return setLoginError(user.error);
+    }
+
+    window.localStorage.setItem("loggedUser", JSON.stringify(user));
+    e.target.reset();
   };
+
+  const isError = () =>
+    errors?.username || errors?.password || loginError ? "block" : "none";
 
   return (
     <main className={styles.main}>
       <div className={styles.form_container}>
         <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+          <div className={styles.logo_wrapper}>
+            <h1 className={styles.logo}>
+              <span className={styles.kino}>Kino</span>
+              <span className={styles.mo}>Mo</span>
+            </h1>
+          </div>
+          <div style={{ display: isError() }} className={styles.errors_wrapper}>
+            <ul>
+              {errors?.username && <li>{errors.username?.message}</li>}
+              {errors?.password && <li>{errors.password?.message}</li>}
+              {loginError && <li>{loginError}</li>}
+            </ul>
+          </div>
           <div className={styles.inputs_container}>
             <div className={styles.input_wrapper}>
               <span className={`${styles.icon} ${styles.icon_username}`}></span>
@@ -38,7 +63,6 @@ const login = () => {
             <div className={styles.input_wrapper}>
               <span className={`${styles.icon} ${styles.icon_password}`}></span>
               <input
-                onFocus={() => console.log("focused")}
                 className={styles.input}
                 {...register("password")}
                 placeholder="Password"
@@ -47,10 +71,20 @@ const login = () => {
             </div>
           </div>
 
-          <div>
-            <input type="submit" value="Sign In" />
+          <div className={styles.submit_wrapper}>
+            <input
+              className={styles.submit_button}
+              type="submit"
+              value="Sign In"
+            />
           </div>
         </form>
+
+        <div className={styles.footer}>
+          <p>
+            Don't have an account? <a href="/auth/signup">Signup now</a>
+          </p>
+        </div>
       </div>
     </main>
   );
