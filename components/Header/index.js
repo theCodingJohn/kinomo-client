2744f20f01bb9dd39f9e8e89095c65f314be545a
John Carlo Cunanan <codingjohn@outlook.com>
2021-05-31 22:14:41 +0800

    Add header logo

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,7 +3,18 @@ import styles from "./header.module.scss";
 const Header = () => {
   return (
     <header className={styles.header}>
-      <nav></nav>
+      <nav className={styles.nav}>
+        <div className={styles.row}>
+          <div className={styles.logo_wrapper}>
+            <p className={styles.logo}>
+              <span className={styles.kino}>Kino</span>
+              <span className={styles.mo}>Mo</span>
+            </p>
+          </div>
+          <div></div>
+          <div></div>
+        </div>
+      </nav>
     </header>
   );
 };
