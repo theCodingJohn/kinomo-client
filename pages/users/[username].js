4260c2ded32cadf73fc1e51cde594f1645f92f94
John Carlo Cunanan <codingjohn@outlook.com>
2021-06-28 10:58:58 +0800

    Add user page backdrop

--- pages/users/[username].js
+++ pages/users/[username].js
@@ -1,14 +1,32 @@
 import userService from "../../services/user.service";
 
 import Layout from "../../components/Layout";
+import Section from "../../components/Section";
+
+import styles from "../../styles/user.module.scss";
+
+import default_banner from "../../public/images/default_banner.jpg";
 
 const profile = ({ user }) => {
+  const backdrop_path =
+    user.movies[user.movies.length - 1]?.movie.backdrop_path;
+  console.log(user);
+
   return (
     <Layout>
-      <div>
-        <h1>Profile</h1>
-        {user?.username}
-      </div>
+      <Section
+        className={styles.hero}
+        style={{
+          backgroundImage: `${
+            backdrop_path
+              ? `url(https://image.tmdb.org/t/p/w1280${backdrop_path})`
+              : `url(${default_banner})`
+          }`,
+        }}
+      >
+        <div className={styles.overlay}></div>
+        <div className={styles.row}></div>
+      </Section>
     </Layout>
   );
 };
